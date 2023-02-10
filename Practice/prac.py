82fdb2592bf699c7ad4797bbbcc5b99c084bf5ba
Nihal <nihalbarhaiyya@gmail.com>
2023-02-10 20:07:21 +0530

    some new file

---
+++ Practice/prac.py
@@ -0,0 +1,26 @@
+# lst = [1,2,3,4,5,6,7,8]
+# d = {}
+# for i in lst:
+#     d[i]="apple"
+# print(d)
+
+# l = [12,3,4,5,'222','asdasd',324235345,[1,2,3,4,]]
+# def t(a):
+#     n = []
+#     for i in a:
+#         if type(i) == int:
+#             n.append(i)
+#         elif type(i)==list:
+#             for j in i:
+#                 n.append(j)
+#         else:
+#             pass
+    
+#     return n
+
+# print(t(l))
+
+
+
+   
+    
